757f548c0d36029e7bfc2defd27c864714375e1e
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-09 02:01:07 -0500

    added formatted ex

---
+++ formatted.rs
@@ -0,0 +1,12 @@
+// This is a program showing formatted text
+
+
+fn main() {
+    // Use '{}' will be replaced with argument
+
+    println!("{} days in some month", 31);
+    
+    println!("{0} this is {1}", "Saul", "Brenda");
+
+    
+}
