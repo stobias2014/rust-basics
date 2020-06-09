38acc56b09f07a0b9ebe80edd1365b7ae639366b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-09 01:54:20 -0500

    added hello

---
+++ hello.rs
@@ -0,0 +1,11 @@
+// This is a comment
+
+
+
+// This is a main function
+fn main() {
+    println!("Hello Saul");
+    println!("You have just started programming in Rust");
+    println!("Welcome to Rust, ok");
+}
+
